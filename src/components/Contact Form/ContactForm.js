d6d9898228fb39b73db8b58916c4ea384d956164
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-01-06 23:07:11 +0600

    complete contact form js

--- src/components/Contact Form/ContactForm.js
+++ src/components/Contact Form/ContactForm.js
@@ -24,3 +24,26 @@ const ContactForm = () => {
         }}
         
         >
+             <h1 className='text-center' style={{marginTop: '25px', color:'#E4E5E6', fontWeight: 'bold', fontSize: "30px"}}>Contact Us</h1>
+            
+            <form className='row' style={{ margin:'25px 85px 75px 100px', color:'#fff'}} onSubmit={sendEmail}>
+                {/* <label>Name</label> */}
+                <input style={{marginTop:'20px'}} type="text" name="name" className="form-control" placeholder="enter your name" />
+
+                {/* <label>Email</label> */}
+                <input style={{marginTop:'20px'}} type="email" name="user_email" className="form-control" placeholder="enter your email address" />
+
+                <label></label>
+                <textArea style={{marginTop:'20px'}} name='message' rows='4' className="form-control" placeholder="enter your messages" />
+                <button
+                 type="submit" 
+                 value="send" 
+                 className="form control btn btn-success"
+                 style={{marginTop:'30px', fontWeight: 600}}
+                 >Send</button>
+            </form>
+        </div>
+    );
+};
+
+export default ContactForm;
